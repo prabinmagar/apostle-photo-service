0e047e32ffd5797748eaa9911b7e2ff6af4264bf
prabin.thapamagar <prabin_mi@yonefu.info>
2023-09-16 17:39:43 +0545

    s

--- src/screens/details/DetailsPage.jsx
+++ src/screens/details/DetailsPage.jsx
@@ -3,7 +3,6 @@ import { imagesData, staticImages } from "../../images";
 import { Button } from "@material-tailwind/react";
 import PropTypes from "prop-types";
 import { List, ListItem } from "@material-tailwind/react";
-import { GalleryItem } from "../admin/dashboard/Gallery";
 import {
   BsCalendarDate,
   BsEye,
@@ -13,6 +12,7 @@ import {
 import { AiOutlineEye, AiOutlineHeart } from "react-icons/ai";
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import Booking from "../../components/common/booking";
 
 const DetailsPage = () => {
   const scrollToTop = () => {
@@ -51,114 +51,119 @@ const DetailsPage = () => {
   ];
 
   return (
-    <div className="flex px-3 py-12 mt-10">
-      <div className="containers w-full bg-white">
-        <div className="grid xl:grid-cols-[auto_340px] gap-12">
-          <div>
-            <div className="px-2 flex items-center justify-between border-b-[1px] border-black/10 py-4 w-full">
-              <div className="flex items-center">
-                <div className="rounded-full w-[40px] h-[40px] overflow-hidden me-3 bg-green-700">
-                  <img
-                    src="/src/assets/images/user.jpg"
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-                <div className="flex flex-col">
-                  <span className="font-semibold text-black/70 font-inter">
-                    Marcus Arthur
-                  </span>{" "}
-                  <span className="text-xs font-inter">@marcus</span>
-                </div>
-              </div>
-              <div>
-                <p className=" font-semibold text-base lg:inline-flex items-center font-inter text-black/70 flex">
-                  <span className="me-3 inline-flex items-center">
-                    <BsEye className="me-2" size={18} />
-                    Views
-                  </span>
-                  <span>120</span>
-                </p>
-                <p className="ms-4 font-semibold text-base lg:inline-flex items-center font-inter text-black/70 flex">
-                  <Link to="/" className="me-3 inline-flex items-center">
-                    <BsShare className="me-2" size={18} />
-                    Share
-                  </Link>
-                </p>
-              </div>
-            </div>
-            <div className="my-4">
-              <ImageGallery items={images.slice(0, 3)} />
-            </div>
-            <div className="image-description border-t-[1px] border-black/5 py-5 px-2">
-              <p className="mb-2 font-semibold text-lg">
-                Charming Beauty of the Nature
-              </p>
-              <p className="font-inter">
-                A beautiful and ancient spring fed canyon which weaves its way
-                throught 400 meter.
-              </p>
-
-              <ul className="mt-4 pt-3 border-t-[1px] border-t-gray-200">
-                <li className="flex items-center my-2">
-                  <span className="me-3 text-gray-800">
-                    <BsCalendarDate size={16} />
-                  </span>
-                  <span className="text-base font-inter text-dark">
-                    Uploaded on June 23, 2023
-                  </span>
-                </li>
-                <li className="flex items-center my-2">
-                  <span className="me-3 text-gray-800">
-                    <BsFillCameraFill size={19} />
-                  </span>
-                  <span className="text-base font-inter text-dark">
-                    Full HD Quality
-                  </span>
-                </li>
-              </ul>
-            </div>
-            <CommentsForm />
-            <Comments />
-          </div>
-          <div className="py-6">
+    <>
+      <div className="flex px-3 py-12 mt-10">
+        <div className="containers w-full bg-white">
+          <div className="grid xl:grid-cols-[auto_340px] gap-12">
             <div>
-              <h3 className="font-inter text-lg font-medium">Categories</h3>
-              <List className="flex xs:flex-row flex-wrap gap-x-5 flex-col">
-                <ListItem className="py-2.5 border-b-2 my-0 border-dotted rounded-none border-gray-200 text-sm sm:text-base w-auto">
-                  Maternity
-                </ListItem>
-                <ListItem className="py-2.5 border-b-2 my-0 border-dotted rounded-none border-gray-200 text-sm sm:text-base w-auto">
-                  Nature
-                </ListItem>
-                <ListItem className="py-2.5 border-b-2 my-0 border-dotted rounded-none border-gray-200 text-sm sm:text-base w-auto">
-                  Landscapes
-                </ListItem>
-                <ListItem className="py-2.5 border-b-2 my-0 border-dotted rounded-none border-gray-200 text-sm sm:text-base w-auto">
-                  Wedding
-                </ListItem>
-                <ListItem className="py-2.5 border-b-2 my-0 border-dotted rounded-none border-gray-200 text-sm sm:text-base w-auto">
-                  Tours & Travels
-                </ListItem>
-                <ListItem className="py-2.5 border-b-2 my-0 border-dotted rounded-none border-gray-200 text-sm sm:text-base w-auto">
-                  Seminars
-                </ListItem>
-                <ListItem className="py-2.5 border-b-2 my-0 border-dotted rounded-none border-gray-200 text-sm sm:text-base w-auto">
-                  New Trends
-                </ListItem>
-              </List>
+              <div className="px-2 flex items-center justify-between border-b-[1px] border-black/10 py-4 w-full">
+                <div className="flex items-center">
+                  <div className="rounded-full w-[40px] h-[40px] overflow-hidden me-3 bg-green-700">
+                    <img
+                      src="/src/assets/images/user.jpg"
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="font-semibold text-black/70 font-inter">
+                      Marcus Arthur
+                    </span>{" "}
+                    <span className="text-xs font-inter">@marcus</span>
+                  </div>
+                </div>
+                <div className="flex items-center flex-wrap">
+                  <p className=" font-semibold lg:inline-flex items-center font-inter text-black/70 flex text-sm">
+                    <span className="me-3 inline-flex items-center">
+                      <BsEye className="me-2" size={18} />
+                      Views
+                    </span>
+                    <span>120</span>
+                  </p>
+                  <p className="ms-4 font-semibold text-sm lg:inline-flex items-center font-inter text-black/70 flex">
+                    <Link to="/" className="inline-flex items-center">
+                      <BsShare className="me-2" size={18} />
+                      Share
+                    </Link>
+                  </p>
+                </div>
+              </div>
+              <div className="my-4">
+                <ImageGallery items={images.slice(0, 3)} />
+              </div>
+              <div className="image-description border-t-[1px] border-black/5 py-5 px-2">
+                <p className="mb-2 font-semibold text-lg">
+                  Charming Beauty of the Nature
+                </p>
+                <p className="font-inter">
+                  A beautiful and ancient spring fed canyon which weaves its way
+                  throught 400 meter.
+                </p>
+
+                <ul className="mt-4 pt-3 border-t-[1px] border-t-gray-200">
+                  <li className="flex items-center my-2">
+                    <span className="me-3 text-gray-800">
+                      <BsCalendarDate size={16} />
+                    </span>
+                    <span className="text-base font-inter text-dark">
+                      Uploaded on June 23, 2023
+                    </span>
+                  </li>
+                  <li className="flex items-center my-2">
+                    <span className="me-3 text-gray-800">
+                      <BsFillCameraFill size={19} />
+                    </span>
+                    <span className="text-base font-inter text-dark">
+                      Full HD Quality
+                    </span>
+                  </li>
+                </ul>
+              </div>
+              <CommentsForm />
+              <Comments />
             </div>
-            <div className="my-6">
-              <h3 className="font-inter text-lg font-medium">Latest Photos</h3>
-              <div className="grid mt-4 grid-cols-2 gap-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-2">
-                {imagesData.slice(0, 8).map((image) => {
-                  return <GalleryItem key={image.id} data={image} />;
-                })}
+            <div className="py-6">
+              <div>
+                <h3 className="font-inter text-lg font-medium">Categories</h3>
+                <List className="flex xs:flex-row flex-wrap gap-x-5 px-0  flex-col">
+                  <ListItem className="py-1 my-0 rounded-none text-sm sm:text-base w-auto">
+                    Maternity
+                  </ListItem>
+                  <ListItem className="py-1 my-0 rounded-none text-sm sm:text-base w-auto">
+                    Nature
+                  </ListItem>
+                  <ListItem className="py-1 my-0 rounded-none text-sm sm:text-base w-auto">
+                    Landscapes
+                  </ListItem>
+                  <ListItem className="py-1 my-0 rounded-none text-sm sm:text-base w-auto">
+                    Wedding
+                  </ListItem>
+                  <ListItem className="py-1 my-0 rounded-none text-sm sm:text-base w-auto">
+                    Tours & Travels
+                  </ListItem>
+                  <ListItem className="py-1 my-0 rounded-none text-sm sm:text-base w-auto">
+                    Seminars
+                  </ListItem>
+                  <ListItem className="py-1 my-0 rounded-none text-sm sm:text-base w-auto">
+                    New Trends
+                  </ListItem>
+                </List>
+              </div>
+              <div className="my-6">
+                <h3 className="font-inter text-lg font-medium">
+                  Latest Photos
+                </h3>
+                <div className="grid mt-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-3">
+                  {imagesData.slice(0, 24).map((image) => {
+                    return <RelatedItem key={image.id} data={image} />;
+                  })}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <Booking />
+    </>
   );
 };
 
@@ -167,7 +172,7 @@ export default DetailsPage;
 export const CommentsForm = () => {
   return (
     <div className="flex flex-col px-3">
-      <h2 className="pt-3 pb-2 text-gray-800 text-lg font-semibold font-inter">
+      <h2 className="pt-3 pb-2 text-dark text-lg font-semibold font-inter">
         Add a new comment
       </h2>
       <form className="w-full">
@@ -209,7 +214,7 @@ export const Comments = () => {
   ];
   return (
     <div className="pb-12 px-3 mt-10">
-      <h2 className="text-gray-800 text-lg font-semibold font-inter mb-6">
+      <h2 className="text-dark text-lg font-semibold font-inter mb-6">
         Some recent comments
       </h2>
       <div className="grid gap-6">
@@ -246,5 +251,21 @@ export const CommentsItem = (props) => {
 };
 
 CommentsItem.propTypes = {
+  data: PropTypes.any,
+};
+
+export const RelatedItem = (props) => {
+  return (
+    <article className="rounded-xl bg-white p-1 hover:transform hover:scale-105 duration-300 ">
+      <a href="#">
+        <div className="relative flex items-end overflow-hidden rounded h-[140px]">
+          <img src={props.data.image} className="w-full h-full object-cover" />
+        </div>
+      </a>
+    </article>
+  );
+};
+
+RelatedItem.propTypes = {
   data: PropTypes.any,
 };
